95567e10d99327e8e7c2e211f3914354f76731e0
Xie <yxie1@MTVL1638015f6.local>
2019-03-11 21:31:36 -0700

    added lecture and homework

--- classes/examples/src/club/makeable/apcs/StandardClass.java
+++ classes/examples/src/club/makeable/apcs/StandardClass.java
@@ -21,9 +21,6 @@ public class StandardClass {
 		System.out.println( acct1.toString() );
 		System.out.println( acct2.toString() );
 
-		Student s1 = new Student("Emily", "Las Lomas", 10);
-		Student s2 = new Student("Emily", "Las Lomas", 10);
-
 		// equals
 		System.out.println( "acct1==acct2: " + (acct1 == acct2) );
 		System.out.println( "acct1 equals acct2: " + acct1.equals(acct2) );
@@ -31,6 +28,9 @@ public class StandardClass {
 		System.out.println( "acct1==acct3: " + (acct1 == acct3) );
 		System.out.println( "acct1 equals acct3: " + acct1.equals(acct3) );
 		System.out.println( "acct1 equals null: " + acct1.equals(null) );
+
+		Student s1 = new Student("Emily", "Las Lomas", 10);
+		Student s2 = new Student("Emily", "Las Lomas", 10);
 
 		System.out.println( "student1==student2: " + (s1 == s2) );
 		System.out.println( "student1 equals student2: " + s1.equals(s2) );		// override equals()
@@ -44,8 +44,8 @@ public class StandardClass {
 		String s1 = "abc";		// string literals. special case for String to be initialized just like primitive type
 		String s2 = new String("abc");
 
-		String s3 = "abc";
-		String s4 = "bc";
+		String s3 = "abc"; 
+		String s4 = "dc"; 
 		String s5 = "a" + "bc";
 		// String s6 = 3+ 4;    // String.valueOf(3 + 4);
 
@@ -64,7 +64,7 @@ public class StandardClass {
 		System.out.println( "ABC compareTo 12345: " + "ABC".compareTo("12345") );
 
 		String alphabet = "abcdefghijklmnopqrstuvwxyz";
-		int length = alphabet.length();
+		int length = alphabet.length(); // alphabet[0]: index (0, length-1)
 		System.out.println("alphabet: length() = " + length );
 		System.out.println("substring(0): " + alphabet.substring(0));
 		System.out.println("substring(length): " + alphabet.substring(length));
@@ -78,6 +78,9 @@ public class StandardClass {
 
 		System.out.println("indexOf(\"fgh\"): " + alphabet.indexOf("fgh"));
 		System.out.println("indexOf(\"fee\"): " + alphabet.indexOf("fee"));
+		
+		System.out.println("\"fghfghfgh\".indexOf(\"fgh\"): " + "fghfghfgh".indexOf("fgh"));
+		
 	}
 
 	private void testWrapperClasses() {
@@ -105,7 +108,14 @@ public class StandardClass {
 		static double sqrt(double x)
 		static double random()  // 0.0 <= result < 1.0
 		*/
+		System.out.println("Math.abs(-3): " + Math.abs(-3));
+		System.out.println("Math.abs(3.0): " + Math.abs(3.0));
+		System.out.println("Math.pow(5.0, 3.2): " + Math.pow(5.0, 3.2));
+		System.out.println("Math.sqrt(5.0): " + Math.sqrt(5.0));
 		
+		// generate a random int between [20,50)
+		int result = (int)(Math.random()*(50-20)) + 20;
+		System.out.println("random(20,50): " + result);
 		
 	}
 	
@@ -114,6 +124,25 @@ public class StandardClass {
 
 		// Student[] studentArray = new Student[10];
 		List<Student> studentList = new ArrayList<Student>();
+		studentList.add(new Student("name1", "school1", 5));
+		studentList.add(new Student("name2", "school2", 6));
+		studentList.add(2, new Student("name3", "school3", 6));
+		
+		System.out.println("List content 1: " + studentList);
+		
+		System.out.println(studentList.size());
+		System.out.println(studentList.get(1));
+		
+		studentList.set(1, new Student("name4", "school4", 8));
+		System.out.println("List content 2: " + studentList);
+
+		studentList.remove(2);
+		System.out.println("List content 3: " + studentList);
+
+		Student[] stype = new Student[0];
+		Student[] students = (Student[]) studentList.toArray();
+		Student[] students2 = studentList.toArray(stype);
+		
 		
 		/*
 			boolean add(E obj);
@@ -132,10 +161,10 @@ public class StandardClass {
 	public static void main(String[] args) {
 		StandardClass std = new StandardClass();
 
-		std.testObjectMethods();
-		std.testStringMethods();
-		std.testWrapperClasses();
-		std.testMathClass();
+//		std.testObjectMethods();
+//		std.testStringMethods();
+//		std.testWrapperClasses();
+//		std.testMathClass();
 		std.testArrayList();
 	}
 
